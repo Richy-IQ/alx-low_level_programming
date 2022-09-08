f24e4aae60c6d6938cb648333b0fcceebd35fad1
Richy-IQ <richardehindero221@gmail.com>
2022-09-08 13:42:31 -0700

    good

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - entry point
  *
- * Return: Always 0 (success)
+ * return: Always 0 (success)
  */
 int main(void)
 {
-
-	puts("\"programming is like building a multiligual puzzle");
-	return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0); 
 }
