8034158e62f4ecd0830c0aca0b772cd1fae04faf
Richy-IQ <richardehindero221@gmail.com>
2022-09-08 07:04:36 -0700

    coding start

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,10 @@
-#include<stdio.h>
-puts("Programming is like building a multilingual puzzle \n");
-return 0;
+#include <stdio.h>
+/**
+ * description: main - print out string to stdout .
+ * return: 0 if sucess.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
+}
