45c086220c5fac4c2cb69e0e56fa7f760d08c968
Richy-IQ <richardehindero221@gmail.com>
2022-09-09 13:01:06 -0700

    mike

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; <= '9'; d++)
+	{
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar9'\n');
+		return (0);
+	}
