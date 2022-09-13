255f583216f626e2a5c16b679e8834b86ce2dc35
Richy-IQ <richardehindero221@gmail.com>
2022-09-13 06:10:59 -0700

    work

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,14 @@
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_purchar('h');
-	_putchar('a');
-	_putchar('r');
-	-putcgar('\n');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
