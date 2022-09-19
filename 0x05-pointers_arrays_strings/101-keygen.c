3c34259c1bba20fbe5910f9f80292f8970b6876d
Richy-IQ <richardehindero221@gmail.com>
2022-09-19 07:42:57 -0700

    root

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,35 +13,41 @@ int main(void)
 	int index = 0, sum = 0, diff_half1, diff_half2;
 
 	srand(time(0));
+
 	while (sum < 2772)
 	{
 		password[index] = 33 + rand() % 94;
 		sum += password[index++];
 	}
+
 	password[index] = '\0';
+
 	if (sum != 2772)
 	{
 		diff_half1 = (sum - 2772) / 2;
 		diff_half2 = (sum - 2772) / 2;
 		if ((sum - 2772) % 2 != 0)
 			diff_half1++;
+
 		for (index = 0; password[index]; index++)
 		{
 			if (password[index] >= (33 + diff_half1))
+			{
 				password[index] -= diff_half1;
-			break;
+				break;
+			}
 		}
-	}
-	for (index = 0; password[index]; index++)
-	{
-		if (password[index] >= (33 + diff_half2))
+		for (index = 0; password[index]; index++)
 		{
-			password[index] -= diff_half2;
-			break;
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
 		}
 	}
-}
-printf("%s", password);
-return (0);
-}
 
+	printf("%s", password);
+
+	return (0);
+}
