becbcad7855065f12c086790ffb569d465bf18ac
Richy-IQ <richardehindero221@gmail.com>
2022-10-20 14:39:45 -0700

    preacher

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,12 +2,12 @@
 
 void msg(void)__attribute__((constructor));
 
-/*
+/**
  * msg - prints a specific message
  *
  * Return: No return
  */
-void msg(void) __attribute__((constructor));
+void msg(void)
 {
 	printf("You're beat! and yet, you must allow,");
 	printf("\nI bore my house upon my back!\n");
