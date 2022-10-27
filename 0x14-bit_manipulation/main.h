1eabf7585f717d0c37e314144549dea6721b8caa
Richy-IQ <richardehindero221@gmail.com>
2022-10-27 14:16:22 -0700

    origin

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
+/*
+ * File: main.h
+ * AUth: richy
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-bit_manipulation directory.
+ */
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -11,5 +15,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
-#endif /* MAIN_H*/
+#endif /* MAIN_H */
