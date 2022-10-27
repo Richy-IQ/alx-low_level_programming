9f655b8b507352cd40672a89f7f3891b1fea0489
Richy-IQ <richardehindero221@gmail.com>
2022-10-27 14:27:45 -0700

    origin

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,18 +1,22 @@
 #include "main.h"
 
 /**
- * get_endianness - Checks the endianness.
+ * flip_bits - Counts the number of bits needed to be
+ *             flipped to get from one number to another.
+ * @n: The number.
+ * @m: The number to flip n to.
  *
- * Return: If big-endian - 0.
- *         If little-endian - 1.
+ * Return: The necessary number of bits to flip to get from n to m.
  */
-int get_endianness(void)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int num = 1;
-	char *endian = (char *)&num;
+	unsigned long int xor = n ^ m, bits = 0;
 
-	if (*endian == 1)
-		return (1);
+	while (xor > 0)
+	{
+		bits += (xor & 1);
+		xor >>= 1;
+	}
 
-	return (0);
+	return (bits);
 }
